b9ac5e58f719cf100301a913aa4ec10db97d72f5
Tushar  Madan <tusharmadan56@gmail.com>
2026-07-12 03:09:04 +0530

    Time: 634 ms (5.13%) | Memory: 22.7 MB (5.45%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int sz;
     vector<int> cuts;
     map<pair<int,int>,int> mp;
     int dfs(int i,int j){
-
-        if(i==j) return 0;
-
         if(mp.find({i,j})!=mp.end()) return mp[{i,j}];
-
-        int ans =1e9;
-
-        for(int k=0;k<sz;k++){
-            if(cuts[k]>i && cuts[k]<j){
-                ans = min(ans , j-i + dfs(i,cuts[k])+dfs(cuts[k],j));
-            }
+        if(j-i<=0) return 0;
+        int ans=1e9;
+        bool f=true;
+        for(int k=i+1;k<j;k++){
+            ans = min(ans , dfs(i,k)+dfs(k,j)+(cuts[j]-cuts[i]));
+            f=false;
         }
+        if(f) return mp[{i,j}]=0;
+        return mp[{i,j}]=ans;
 
-        if(ans == 1e9) return 0;
-
-        return mp[{i,j}]=ans; 
     }
     int minCost(int n, vector<int>& cuts) {
-        this->sz = cuts.size();
-        this->cuts = cuts;
+        
+        cuts.push_back(0);
+        cuts.push_back(n);
 
-        return dfs(0,n);
+        sort(cuts.begin(),cuts.end());
+        this->cuts=cuts;
+        return dfs(0,cuts.size()-1);
     }
 };
