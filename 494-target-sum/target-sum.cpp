28aa90abf952248c5e8816e3929383fbbeb5af1c
Tushar  Madan <tusharmadan56@gmail.com>
2026-07-12 02:16:27 +0530

    Time: 615 ms (19.34%) | Memory: 65.5 MB (5.01%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -3,29 +3,36 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(40005,0));
+        vector<int> dp(40005,0),prev(40005,0);
 
         
 
         const int offset = 20000;
-        dp[0][offset] = 1;
+        //dp[0][offset] = 1;
+
+        prev[offset] = 1;
 
 
         for(int i=1;i<=n;i++){
+            //dp = prev;
+
+            //dp.fill(dp.begin(),dp.end(),0);
             for(int j=-20000;j<=20000;j++){
                 int ans = 0;
 
                 
 
 
-                if(j-nums[i-1]>=-offset) dp[i][j+offset] += dp[i-1][j-nums[i-1]+offset];
-                if(j+nums[i-1]<=20000) dp[i][j+offset] += dp[i-1][j+nums[i-1]+offset];
+                if(j-nums[i-1]>=-offset) dp[j+offset] = prev[j-nums[i-1]+offset];
+                if(j+nums[i-1]<=20000) dp[j+offset] += prev[j+nums[i-1]+offset];
                 
             }
+
+            prev = dp;
 
 
         }
 
-        return dp[n][target+offset];
+        return dp[target+offset];
     }
 };
