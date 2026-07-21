9cac8f8a5bca7fee3efda827de0031acf4a1a7c7
Tushar  Madan <tusharmadan56@gmail.com>
2026-07-21 14:11:48 +0530

    Time: 837 ms (6.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 178-rank-scores/rank-scores.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select score,
+
+(select count(distinct s2.score) from Scores s2 
+    where s2.score>=s1.score
+    ) as 'rank'
+
+from Scores s1 order by s1.score DESC
