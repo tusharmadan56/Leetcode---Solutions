02aead64c08284f115e9ff9504e0fa1512d287a9
Tushar  Madan <tusharmadan56@gmail.com>
2026-07-21 22:07:14 +0530

    Time: 371 ms (11.33%) | Memory: 0B (100.00%) - LeetSync

--- 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
+++ 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
@@ -4,9 +4,8 @@
 with f1 as (
     select user_id,reaction,count(distinct content_id) as 'c1' from reactions group by user_id,reaction 
 ),
-
- f2 as (    
-    select user_id,count(distinct content_id) as 'c' from reactions group by user_id having c>=5
+f2 as (
+    select user_id , reaction , c1 , sum(c1) over(partition by user_id) as total_count from f1
 )
 
-select a.user_id , a.reaction as 'dominant_reaction' , Round(a.c1/b.c,2) as 'reaction_ratio' from f1 a join f2 b on a.user_id = b.user_id where Round(a.c1/b.c,2) > 0.6 order by Round(a.c1/b.c,2) desc;
+select user_id , reaction as 'dominant_reaction' , Round(c1/total_count,2) as 'reaction_ratio' from f2   where total_count >= 5 and Round(c1/total_count,2)>=0.6 order by  Round(c1/total_count,2) desc , user_id ;
