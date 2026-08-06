e962a8d427915201e4b633278ea1f46204b50a33
Tushar  Madan <tusharmadan56@gmail.com>
2026-08-07 03:25:37 +0530

    Time: 11 ms (83.02%) | Memory: 19.2 MB (44.26%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) return NULL;
+        queue<Node*> q;
+        q.push(root);
+
+        while(q.size()){
+            int s = q.size();
+
+            for(int i=0;i<s;i++){
+                Node* f = q.front();
+                q.pop();
+                if(i==s-1){
+                    f->next = NULL;
+                }
+                else{
+                    f->next = q.front();
+                }
+
+                if(f->left) q.push(f->left);
+                if(f->right) q.push(f->right);
+            }
+        }
+
+        return root;
+    }
+};
