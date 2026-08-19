f971aacb8ce8581157a3ac83dc56d65d92615e0d
Tushar  Madan <tusharmadan56@gmail.com>
2026-08-19 16:47:45 +0530

    Time: 369 ms (9.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+
+with cte as (select a1.machine_id,a1.process_id,round(a2.timestamp - a1.timestamp , 3) as diff from activity a1 join activity a2 on a1.machine_id=a2.machine_id and a2.process_id=a1.process_id where a1.activity_type = 'start' and a2.activity_type = 'end')
+
+select machine_id , round(avg(diff),3) as processing_time from cte group by machine_id;
