0bb3f295ac4cc0e21003f08a5c3a612e63e3524b
Tushar  Madan <tusharmadan56@gmail.com>
2026-08-23 03:54:08 +0530

    Time: 1444 ms (7.76%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -2,9 +2,9 @@
 
 
 with cte as (
-    select *   from products p1 where change_date <= '2019-08-16' and change_date = (
-    select max(change_date) from products p2 where p2.product_id = p1.product_id and p2.change_date <= '2019-08-16'
-)
+    select *   from products p1 where change_date = (
+        select max(change_date) from products p2 where p2.product_id = p1.product_id and p2.change_date <= '2019-08-16'
+    )
 ),
 
 p as (
