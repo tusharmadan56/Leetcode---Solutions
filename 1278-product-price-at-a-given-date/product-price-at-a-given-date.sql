821a591ce33c9fa59573284c08932d747e00b6f8
Tushar  Madan <tusharmadan56@gmail.com>
2026-08-23 03:52:27 +0530

    Time: 962 ms (11.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+
+with cte as (
+    select *   from products p1 where change_date <= '2019-08-16' and change_date = (
+    select max(change_date) from products p2 where p2.product_id = p1.product_id and p2.change_date <= '2019-08-16'
+)
+),
+
+p as (
+    select distinct product_id from products
+)
+
+select p.product_id , ifnull(cte.new_price , 10) as price from p left join cte on p.product_id = cte.product_id;
+
+#select * from p;
+
+-- select * from products p left join cte c on p.product_id = c.product_id ;
