af3bda5a23708ca967b501008d5be173449cefe4
Tushar  Madan <tusharmadan56@gmail.com>
2026-08-28 16:52:04 +0530

    Time: 6 ms (4.65%) | Memory: 26 MB (56.22%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int fans = 0;
+        int n  = height.size();
 
-        int n = height.size();
 
-        vector<int> pref(n,0),suff(n,0);
+        int l = 0 , r = n-1;
+        int lmax = 0 , rmax = 0;
+        int ans = 0;
+        while(l<r){
+            if(height[l]<=height[r]){
+                lmax = max(lmax , height[l]);
+                rmax = max(rmax , height[r]);
 
-        pref[0] = height[0];
+                ans+= (lmax - height[l]);
+                l++;
+            }
+            else{
+                lmax = max(lmax , height[l]);
+                rmax = max(rmax , height[r]);
 
-        for(int i=1;i<n;i++){
-            pref[i] = max(pref[i-1], height[i]);
-        }
-
-        suff[n-1] = height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suff[i] = max(suff[i+1] , height[i]);
-        }
-
-        for(int i=1;i<n-1;i++){
-            int ma = min(pref[i],suff[i]);
-
-            if(ma>height[i]){
-                fans+=(ma-height[i]);
+                ans+= (rmax - height[r]);
+                r--;
             }
         }
-        return fans;
+
+        return ans;
     }
 };
