2100a703195bb5b9e56515647b0155d9aced8c56
Tushar  Madan <tusharmadan56@gmail.com>
2026-08-30 23:35:33 +0530

    Time: 77 ms (42.30%) | Memory: 131.4 MB (69.82%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -10,7 +10,9 @@ public:
         }
 
         sort(v.begin(),v.end(),[&](const pair<int,int> a, const pair<int,int> b){
-            
+            if(a.first==b.first){
+                return a.second>b.second;
+            }
             return a.first<b.first;
         });
 
